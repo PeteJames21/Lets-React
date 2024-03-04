2953f59bda3e873cc4b02bbc2cb4a3589e98dedf
PeteJames21 <peetjames21@gmail.com>
2024-03-04 18:09:29 +0300

    Add comment on need for using relative imports in custom modules

--- ES6-Tutorial/custom_modules_2.js
+++ ES6-Tutorial/custom_modules_2.js
@@ -1,5 +1,6 @@
 // NOTE: The file extension is explicitly specified. Failure to add the extension
-// will result in an import error if using the ES import system
+// will result in an import error if using the ES import system. The path must also
+// be relative if importing from a custom module.
 import { Person } from './custom_modules_1.js';
 
 const person = new Person('Miriam');
